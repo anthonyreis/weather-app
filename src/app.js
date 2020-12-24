a5a43895f3e8ffbdf650b8d3fd0d7487b3f52cd9
anthony <matheus.anthony1@gmail.com>
2020-12-24 20:46:53 -0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 8081
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -95,6 +96,6 @@ app.get('/*', (req, res) => {
     })
 })
 
-app.listen(8081, () => {
-    console.log('Server is up on port 8081')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
